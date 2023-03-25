ca0864bc774c0c5ca106d73433d43b95e79b6eeb
pikachua7 <atharvapaliwal7@gmail.com>
2023-03-25 22:20:00 +0530

    add tokenAddress from zkSync

---
+++ frontend/src/utils/TokenAddresses.js
@@ -0,0 +1,27 @@
+export const TOKEN_ADDRESS = [
+    {
+        address: "0x0000000000000000000000000000000000000000",
+        symbol: "ETH",
+        decimal: 18
+    },
+    {
+        address: "0x0faF6df7054946141266420b43783387A78d82A9",
+        symbol: "USDC",
+        decimal: 6
+    },
+    {
+        address: "0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b",
+        symbol: "DAI",
+        decimal: 18
+    },
+    {
+        address: "0x40609141Db628BeEE3BfAB8034Fc2D8278D0Cc78",
+        symbol: "LINK",
+        decimal: 18
+    },
+    {
+        address: "0x0BfcE1D53451B4a8175DD94e6e029F7d8a701e9c",
+        symbol: "wBTC",
+        decimal: 8
+    },
+];
