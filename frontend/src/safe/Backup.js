e160365539007b8c198b6fc84c2c689d231e38dc
pikachua7 <atharvapaliwal7@gmail.com>
2023-03-26 01:18:29 +0530

    add Backup owner functionality

---
+++ frontend/src/safe/Backup.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { TextField, Button } from '@mui/material';
+
+export default function BackUp({ cryptoSafeContract }) {
+    const [backupAddress, setBackupAddress] = useState("");
+    const [newBackupAddress, setNewBackupAddress] = useState("");
+    const [showSetBackupAddress, setShowSetBackupAddress] = useState(false);
+
+    useEffect(() => {
+        if (cryptoSafeContract) {
+            getBackupOwnerAddress();
+        }
+    }, [cryptoSafeContract])
+
+    const getBackupOwnerAddress = async () => {
+        const address = await cryptoSafeContract.getSafeBackupOwner();
+        setBackupAddress(address);
+    }
+
+    const setSafeBackupOwner = async () => {
+        const txHandle = await cryptoSafeContract.setSafeBackupOwner(newBackupAddress, {
+            customData: {
+                // Passing the token to pay fee with
+                feeToken: "0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b",
+            },
+        });
+
+        await txHandle.wait();
+        setShowSetBackupAddress(false);
+        getBackupOwnerAddress();
+    }
+    return (
+        <div>
+            <h2>Set Your Backup Address</h2>
+            <p>
+                {backupAddress} <Button variant="contained" onClick={() => setShowSetBackupAddress(!showSetBackupAddress)}>{showSetBackupAddress ? 'Cancel' : 'Edit'}</Button
+                ></p>
+            {showSetBackupAddress &&
+                <>
+                    <TextField label="BackupAddress" variant="outlined" size='small' value={newBackupAddress} onChange={(e) => setNewBackupAddress(e.target.value)} />
+                    <Button variant="contained" onClick={setSafeBackupOwner}>Update</Button>
+                </>}
+        </div>
+    )
+}
